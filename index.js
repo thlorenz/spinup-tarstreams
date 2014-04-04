407506d0434a2a5655111c3514a89151467474c9
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-04 12:13:26 -0400

    first pass at cleaning up images creation

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
+'use strict';
+var runnel    = require('runnel')
+  , dockerode = require('dockerode')
+  , xtend     = require('xtend')
+  , logEvents = require('./lib/log-events')
+  , Images    = require('./lib/images')
 
-// dockerify-github-repo to get all tar streams
+function imageName(group, id) {
+  return group + ':' id;
+}
 
-// create image for each
+function buildImages(images, streamfns, opts, cb) {
+  var tasks = Object.keys(streamfns)
+    .map(function (k) {
+      var image = imageName(opts.group, k)
+        , streamfn = streamfns[k];
+
+      return function (cb_) {
+        images.build(streamfn(), image, cb_);
+      }
+    });
+
+  runnel(tasks.concat(cb));
+}
+
+function createDocker(opts) {
+  var dockerhost = opts.dockerhost 
+    , parts      = dockerhost.split(':')    
+    , host       = parts.slice(0, -1).join(':').replace(/^tcp/, 'http')
+    , port       = parts[parts.length - 1]
+
+  return dockerode({ host: host, port: port });
+}
+
+var defaultOpts = { 
+    dockerhost: process.env.DOCKER_HOST || 'tcp://127.0.0.1:4243'
+  , group: 'ungrouped' 
+};
+
+
+  /*images
+    .on('processing' , function (info) { log.silly  ('images', 'processing\n', inspect(info)); })
+    .on('building'   , function (info) { log.verbose('images', 'building\n', inspect  (info)); })
+    .on('built'      , function (info) { log.info   ('images', 'built\n', inspect     (info)); })
+    .on('msg'        , function (msg)  { log.silly  ('images', 'msg', msg); })
+    .on('warn'       , function (err)  { log.warn   ('images', 'warn', err); })
+    .on('error'      , function (err)  { log.error  ('images', 'error', err); });*/
+
+var go = module.exports = function (streamfns, opts) {
+  opts = xtend(defaultOpts, opts);
+  opts.docker = opts.docker || createDocker(opts)
+
+  var images = new Images(opts.docker);
+  logEvents(images);
+
+  buildImages(images, streamfns, opts, function (err, res) {
+    if (err) return console.error(err);
+    console.log(res);  
+  });
+}
 
 // create container for each
 
@@ -12,3 +68,8 @@
 // provide a way to remove all containers for a group
 
 // provide a way to remove all images for a group
+
+// Test
+if (!module.parent && typeof window === 'undefined') {
+  
+}
