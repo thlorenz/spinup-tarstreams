01da991cbedb0311a9a3ab3c90da471d8b23f2a1
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-14 21:23:22 +0000

    adding local tar stream fixtures to speed up tests

---
+++ test/fixtures/test-uno/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337);
+
+console.log('Server running at http://localhost:1337/');
